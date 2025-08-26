908fe90511025bf13d72cfa505e69cef69d8255e
Anuj1143 <aanujmaurya8127@gmail.com>
2025-08-26 17:26:16 +0530

    Smart_Grocery_tracker_App

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:smart_grocery_tracker/core/theme/app_text_theme.dart';
+import 'package:smart_grocery_tracker/core/theme/elevated_button_theme.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: AppTextTheme.lightTextTheme,
+    elevatedButtonTheme: AppElevatedButtonTheme.lightElevatedButtonTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: AppTextTheme.darkTextTheme,
+    elevatedButtonTheme: AppElevatedButtonTheme.darkElevvatedButtonTheme,
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:smart_grocery_tracker/core/theme/app_theme.dart';
+import 'package:smart_grocery_tracker/view/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: const HomePage(),
+    );
+  }
+}
